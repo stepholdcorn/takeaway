f074a72fad44fd4d628796e11490a01395ee7980
stepholdcorn <stephieoldcorn@gmail.com>
2014-12-19 16:36:29 +0000

    Adding a simple menu

---
+++ lib/menu.rb
@@ -0,0 +1,15 @@
+module Menu
+
+	def show_menu
+		menu
+	end
+
+	def menu
+		menu = [ {name: 'margherita', price: 8} , 
+				{name: 'pepperoni', price: 10} ,
+				{name: 'hawaiian', price: 10} ,
+				{name: 'bbq', price: 11} ,
+				{name: 'feast', price: 12} ]
+	end
+
+end
---
+++ spec/menu_spec.rb
@@ -0,0 +1,13 @@
+require 'menu'
+
+class List; include Menu; end
+
+describe Menu do 
+
+	let(:menu) {List.new}
+
+	it 'should contain a list of products' do
+		expect(menu.show_menu).to include(:name => "pepperoni", :price => 10)
+	end
+	
+end
