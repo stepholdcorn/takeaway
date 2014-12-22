87c6c04765e91a9e0acfd84d60b70dd0fff03db5
stepholdcorn <stephieoldcorn@gmail.com>
2014-12-22 10:59:21 +0000

    Allowing a customer to place an order

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,7 +1,19 @@
 class Customer
 
+	def initialize
+		@order_placed = false
+	end
+
 	def view_menu(menu)
 		menu.show_menu
+	end
+
+	def place_order!
+		@order_placed = true
+	end
+
+	def order_placed?
+		@order_placed
 	end
 
 end
--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -9,4 +9,9 @@ describe Customer do
 		expect(menu).to receive(:show_menu)
 		customer.view_menu(menu)
 	end
+
+	it 'should be able to place an order' do
+		customer.place_order!
+		expect(customer.order_placed?).to eq(true)
+	end
 end
