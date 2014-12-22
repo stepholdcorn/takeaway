7ed8f79ed1179027326bc723cc6884ba84ced8ab
stepholdcorn <stephieoldcorn@gmail.com>
2014-12-22 11:13:19 +0000

    Allowing a customer to order a specific pizza

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,11 +1,22 @@
 class Customer
 
 	def initialize
+		@selection = []
 		@order_placed = false
 	end
 
 	def view_menu(menu)
 		menu.show_menu
+	end
+
+	def make_selections
+		puts "Please enter a pizza"
+		selection = gets.chomp
+		@selection << selection
+	end
+
+	def order_review
+		@selection
 	end
 
 	def place_order!
--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -14,4 +14,9 @@ describe Customer do
 		customer.place_order!
 		expect(customer.order_placed?).to eq(true)
 	end
+
+	it 'should be able to order a bbq pizza' do
+		customer.make_selections
+		expect(customer.order_review[0]).to eq('bbq')
+	end
 end
