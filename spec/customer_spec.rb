ee5dc71c1fa08b679870078298dab3c35a2ca18a
stepholdcorn <stephieoldcorn@gmail.com>
2015-01-05 14:47:08 +0000

    Adding mocks for STDIN

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -3,7 +3,7 @@ require 'customer'
 describe Customer do 
 
 	let(:customer) {Customer.new}
-	let(:restaurant) {double :restaurant, receive_order!: true, menu: {bbq: 11}}
+	let(:restaurant) {double :restaurant, receive_order!: true}
 
 
 	it 'should be able to view the menu' do
@@ -18,30 +18,29 @@ describe Customer do
 
 	xit 'should not allow an item to be ordered if it is not on the menu' do
 		customer.make_selections(restaurant)
-		expect(customer.collect_order).to raise_error(false)
+		expect(customer.collect_order).to raise_error('Not on the menu')
 	end
 
 	it 'should be able to order one bbq pizza' do
+		STDIN.should_receive(:gets).and_return("bbq", "1", "", "1")
 		customer.make_selections(restaurant)
 		expect(customer.order_review).to eq([name: 'bbq', quantity: 1])
 	end
 
-	it 'should be able to enter the quantity' do
-		customer.make_selections(restaurant)
-		expect(customer.order_review).to eq([name: 'bbq', quantity: 2])
-	end
-
 	it 'should be able to enter the total number of pizzas selected to place order' do
+		STDIN.should_receive(:gets).and_return("bbq", "1", "", "1")
 		customer.make_selections(restaurant)
 		expect(customer.order_placed?).to eq(true)
 	end
 
 	it 'should not place the order if the total does not match the input' do
+		STDIN.should_receive(:gets).and_return("bbq", "1", "", "2")
 		customer.make_selections(restaurant)
 		expect(customer.order_placed?).to eq(false)
 	end
 
 	it 'should allow more than one type of pizza to be ordered' do
+		STDIN.should_receive(:gets).and_return("bbq", "1", "feast", "1", "", "2")
 		customer.make_selections(restaurant)
 		expect(customer.order_review.count).to eq(2)
 	end
